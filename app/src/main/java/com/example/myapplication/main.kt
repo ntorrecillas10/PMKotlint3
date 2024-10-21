59c17daa2594c4ad9788b1aa1b8882e89d1603a5
ntorrecillas10 <nalascobic7@gmail.com>
2024-10-21 19:28:20 +0200

    ee6

--- app/src/main/java/com/example/myapplication/main.kt
+++ app/src/main/java/com/example/myapplication/main.kt
@@ -8,6 +8,20 @@ fun main() {
     println(ej4evaluarCalificacion(40))
     println(ej5sumarNumeros(1, 2, 3))
     println(ej6invertirArray(arrayOf(1, 2, 3, 4, 5)).joinToString(", "))  // Imprime: 5, 4, 3, 2, 1
+    println(ej7agregarFrutaADLista(listOf("Manzana", "Banana", "Naranja"), "Fresa"))  // Imprime: [Manzana, Banana, Naranja, Fresa]
+    ej8eliminarColorDeLista()
+    ej9imprimirNumeros()
+    println("")
+    ej10imprimirLista(listOf(1, 2, 3, 4, 5))
+    println("")
+    ej11imprimirNumerosHastaN(5)
+    println("")
+    /*ej12imprimirNumerosPositivos()*/
+    println(ej13esPrimo(17))
+
+    val primoMayorQue100 = ej14encontrarPrimoMayorQue100()
+    println(primoMayorQue100)  // Imprime: 101
+
 
 
 }
@@ -59,5 +73,93 @@ como argumento y devuelva un nuevo array con los elementos en orden inverso.*/
 fun ej6invertirArray(numeros: Array<Int>): Array<Int> {
     return numeros.reversedArray()
 }
+
+/*7. Crea una lista inmutable de nombres de
+frutas y agrega una fruta adicional a la lista.*/
+
+fun ej7agregarFrutaADLista(frutas: List<String>, frutaAdicional: String): List<String> {
+    return frutas + frutaAdicional
+}
+
+/*8. Utiliza una lista mutable para almacenar
+nombres de colores y elimina un color de la lista*/
+
+fun ej8eliminarColorDeLista() {
+    val colores = mutableListOf("Rojo", "Verde", "Azul", "Amarillo")
+    colores.remove("Verde")
+    println(colores)  // Imprime: [Rojo, Azul, Amarillo]
+}
+
+/*9. Escribe una función llamada imprimirNumeros que
+utilice un bucle for para imprimir los números del 1 al 10*/
+
+fun ej9imprimirNumeros() {
+    for (numero in 1..10) {
+        print(numero)
+    }
+}
+
+/*10. Define una función llamada imprimirLista que tome una
+lista de números como argumento y utilice un bucle forEach
+para imprimir cada número en la lista.*/
+
+fun ej10imprimirLista(numeros: List<Int>) {
+    numeros.forEach { numero ->
+        print(numero)
+    }
+}
+
+/*11. Implementa una función llamada imprimirNumerosHastaN
+que tome un número como argumento y utilice un bucle while
+para imprimir los números del 1 al número dado.*/
+
+fun ej11imprimirNumerosHastaN(n: Int) {
+    var i = 1
+    while (i <= n) {
+        println(i)
+        i++
+    }
+}
+
+/*12. Crea una función llamada imprimirNumerosPositivos que utilice un
+bucle do-while para imprimir números positivos hasta que se alcance un número negativo.*/
+
+/*fun ej12imprimirNumerosPositivos() {
+    var numero: Int
+
+    do {
+        println("Introduce un número (negativo para terminar):")
+        numero = readLine()?.toIntOrNull() ?: 0
+        if (numero >= 0) {
+            println(numero)
+        }
+    } while (numero >= 0)
+}*/
+
+/*13. Define una función llamada esPrimo que tome un número como argumento y devuelva
+true si es primo y false si no lo es. Utiliza una declaración return para salir de
+la función una vez que se determine si el número es primo.*/
+
+fun ej13esPrimo(numero: Int): Boolean {
+    if (numero <= 1) return false
+    for (i in 2..Math.sqrt(numero.toDouble()).toInt()) {
+        if (numero % i == 0) return false
+    }
+    return true
+}
+
+fun ej14encontrarPrimoMayorQue100(): Int? {
+    for (numero in 101..Int.MAX_VALUE) {
+        if (ej13esPrimo(numero)) {
+            return numero
+        }
+    }
+    return null
+}
+
+
+
+
+
 
 
